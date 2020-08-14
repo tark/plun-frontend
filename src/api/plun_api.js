4d536b567bf92d0ec594694911ee6105dc1c8474
airon@tark.pro <airon@tark.pro>
2020-08-14 21:10:42 +0700

    plun_api.js: refactor

--- src/api/plun_api.js
+++ src/api/plun_api.js
@@ -4,17 +4,29 @@ import {UnauthorizedError} from './unauthorized_error';
 
 const baseApiUrl = 'http://localhost:3001';
 
-export const authToAzureByCode = async () => {
-  const search = window.location.search.replace('?', '')
-  console.log(`search - ${JSON.stringify(queryString.parse(search), null, 2)}`)
-  const {code} = queryString.parse(search)
-  const response = await post('auth-by-code', {code})
-  return response.data;
+export const authToAzureByCode = async (code) => {
+  return post('auth-by-code', {code})
 }
 
 export const authToAzureByUserId = async (userId) => {
-  const result = await post('auth', {userId})
-  console.log(`authToAzureByUserId - after`)
+  return post('auth', {userId})
+}
+
+export const getOrganizations = async (userId) => {
+  return get('organizations', {userId})
+}
+
+export const getProjects = async (userId) => {
+  return get('projects', {userId})
+}
+
+export const getSuggestions = async (query) => {
+  const result = await get('tasks-suggestions', {query})
+  return result.data
+}
+
+const post = async (endpoint, body) => {
+  const result = await axios.post(`${baseApiUrl}/${endpoint}`, body)
   switch (result.status) {
     case 401:
       throw new UnauthorizedError()
@@ -22,18 +34,17 @@ export const authToAzureByUserId = async (userId) => {
     default:
       return result.data;
   }
-}
 
-export const getSuggestions = async (query) => {
-  console.log(`getSuggestions`)
-  const result = await get('tasks-suggestions', {query})
-  return result.data
-}
-
-const post = async (endpoint, body) => {
-  return axios.post(`${baseApiUrl}/${endpoint}`, body)
 }
 
 const get = async (endpoint, params) => {
-  return axios.get(`${baseApiUrl}/${endpoint}`, params)
+  const result = await axios.get(`${baseApiUrl}/${endpoint}`, params)
+  switch (result.status) {
+    case 401:
+      throw new UnauthorizedError()
+    case 200:
+    default:
+      return result.data;
+  }
+
 }
