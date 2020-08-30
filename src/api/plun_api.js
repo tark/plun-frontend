a9a0e234cc2cdc7d77f21a20c254171d6be8b34c
Airon <airon@tark.pro>
2020-08-30 16:17:37 +0700

    plun_api.js: implement planTasks, add logs

--- src/api/plun_api.js
+++ src/api/plun_api.js
@@ -4,28 +4,46 @@ import {UnauthorizedError} from './unauthorized_error';
 
 const baseApiUrl = 'http://localhost:3001';
 
-export const authToAzureByCode = async (code) => {
-  return post('auth-by-code', {code})
+export const getProfile = async (token) => {
+  console.log('getProfile')
+  return get('profile', {token})
+}
+
+export const getToken = async (authCode) => {
+  console.log(`getToken`)
+  return post('token', {authCode})
 }
 
 export const authToAzureByUserId = async (userId) => {
+  console.log(`authToAzureByUserId - ${userId}`)
   return post('auth', {userId})
 }
 
-export const getOrganizations = async (userId) => {
-  return get('organizations', {userId})
+export const getOrganizations = async (token) => {
+  console.log(`getOrganizations - ${token}`)
+  return get('organizations', {token})
 }
 
-export const getProjects = async (userId) => {
-  return get('projects', {userId})
+export const getProjects = async (organizationName, token) => {
+  console.log(`getProjects - ${token}`)
+  return get('projects', {organizationName, token})
 }
 
-export const getSuggestions = async (query) => {
-  const result = await get('tasks-suggestions', {query})
-  return result.data
+export const getSuggestions = async (organizationName, projectName, query, token) => {
+  console.log(`getSuggestions - ${organizationName}, ${projectName}, ${query}`)
+  const result = await get('tasks-suggestions', {organizationName, projectName, query, token})
+  console.log(`getSuggestions - ${result}`)
+  return result
+}
+
+export const planTasks = async (tasks) => {
+  const result = await post('plan-tasks', {tasks})
+  console.log(`planTasks - ${result}`)
+  return result
 }
 
 const post = async (endpoint, body) => {
+  console.log(`post - ${JSON.stringify(body)}`)
   const result = await axios.post(`${baseApiUrl}/${endpoint}`, body)
   switch (result.status) {
     case 401:
@@ -38,7 +56,8 @@ const post = async (endpoint, body) => {
 }
 
 const get = async (endpoint, params) => {
-  const result = await axios.get(`${baseApiUrl}/${endpoint}`, params)
+  console.log(`get - ${JSON.stringify(params)}`)
+  const result = await axios.get(`${baseApiUrl}/${endpoint}`, {params})
   switch (result.status) {
     case 401:
       throw new UnauthorizedError()
