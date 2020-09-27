96120f11cb8f81dd13abfeb7d842c1b4f129b25c
Airon <airon@tark.pro>
2020-09-27 20:26:36 +0700

    plun_api.js: implement few endpoints

--- src/api/plun_api.js
+++ src/api/plun_api.js
@@ -37,27 +37,65 @@ export const getSuggestions = async (organizationName, projectName, query, token
 }
 
 export const planTasks = async (tasks) => {
-  const result = await post('plan-tasks', {tasks})
+  const result = await post('plun', {tasks})
   console.log(`planTasks - ${result}`)
   return result
+}
+
+export const getPlanForToday = async (organizationName, projectName, token) => {
+  const result = await get('plun', {
+    organizationName,
+    projectName,
+    time: new Date().getTime(),
+    token,
+  })
+  console.log(`getPlanForToday - ${result}`)
+  return result
+}
+
+export const getPreviousNearestPlan = async (organizationName, projectName, token) => {
+  const result = await get('plun', {
+    organizationName,
+    projectName,
+    token,
+  })
+  console.log(`getPreviousNearestPlan - ${result}`)
+  return result
+}
+
+export const deleteTask = async (task) => {
+  return del('plun', {taskId: task.id})
+}
+
+export const updateTask = async (task) => {
+  return patch('plun', {task})
 }
 
 const post = async (endpoint, body) => {
   console.log(`post - ${JSON.stringify(body)}`)
   const result = await axios.post(`${baseApiUrl}/${endpoint}`, body)
-  switch (result.status) {
-    case 401:
-      throw new UnauthorizedError()
-    case 200:
-    default:
-      return result.data;
-  }
+  return checkAndReturn(result)
+}
 
+const del = async (endpoint, body) => {
+  console.log(`del - ${JSON.stringify(body)}`)
+  const result = await axios.delete(`${baseApiUrl}/${endpoint}`, {data: body})
+  return checkAndReturn(result)
+}
+
+const patch = async (endpoint, body) => {
+  console.log(`patch - ${JSON.stringify(body)}`)
+  const result = await axios.patch(`${baseApiUrl}/${endpoint}`, body)
+  return checkAndReturn(result)
 }
 
 const get = async (endpoint, params) => {
   console.log(`get - ${JSON.stringify(params)}`)
   const result = await axios.get(`${baseApiUrl}/${endpoint}`, {params})
+  return checkAndReturn(result)
+}
+
+const checkAndReturn = (result) => {
   switch (result.status) {
     case 401:
       throw new UnauthorizedError()
@@ -65,5 +103,4 @@ const get = async (endpoint, params) => {
     default:
       return result.data;
   }
-
 }
