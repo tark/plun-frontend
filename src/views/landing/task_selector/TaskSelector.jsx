f3981211e8f9a287ecc7b6b66f8e19606767139d
Airon <airon@tark.pro>
2020-09-27 20:29:05 +0700

    TaskSelector.jsx: implementing adding new task

--- src/views/landing/task_selector/TaskSelector.jsx
+++ src/views/landing/task_selector/TaskSelector.jsx
@@ -1,10 +1,13 @@
 import React, {useEffect, useRef, useState, useMemo, useCallback} from 'react'
 import {func, string} from 'prop-types'
 import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
+import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
 import './task_selector.css';
 import debounce from 'lodash/debounce';
 import {getSuggestions} from '../../../api/plun_api';
+
+// todo understand why filter options returns empty tasks list
+const filter = createFilterOptions();
 
 export default function TaskSelector(props) {
 
@@ -40,17 +43,37 @@ export default function TaskSelector(props) {
   return <div className='task-selector-root'>
     <Autocomplete
       options={tasks}
-      getOptionLabel={(task) => task.name || task.azureName}
+      getOptionLabel={(task) => {
+        if (task.newTask) {
+          return `Add "${task.name}"`;
+        }
+
+        return task.name;
+      }}
       renderInput={(params) => <TextField
         {...params}
         label='Add task'
       />}
-      filterOptions={x => x}
+      filterOptions={(options, params) => {
+        const filtered = filter(options, params);
+
+        // Suggest the creation of a new value
+        if (params.inputValue !== '') {
+          filtered.push({
+            newTask: true,
+            name: params.inputValue,
+          });
+        }
+
+        return filtered;
+      }}
       onInputChange={(event, newInputValue) => {
         onInputChange(newInputValue)
       }}
       onChange={handleTaskSelect}
       blurOnSelect
+      freeSolo
+      handleHomeEndKeys
     />
   </div>
 
