b6cdf575b4975046ba6700070735fab6ce12f4a6
Airon <airon@tark.pro>
2020-11-11 20:26:30 +0700

    loader.tsx: implement new cool UI

--- src/views/components/loader.tsx
+++ src/views/components/loader.tsx
@@ -1,11 +1,54 @@
-import CircularProgress from '@material-ui/core/CircularProgress';
+import CircularProgress, {CircularProgressProps} from '@material-ui/core/CircularProgress';
 import React from 'react';
+import {makeStyles} from '@material-ui/styles';
+import {createStyles} from '@material-ui/core';
 
-export default function Loader(props: any) {
-  return <div style={{margin: 10}}>
-    <CircularProgress
-      thickness={2}
-      size={20}
-    />
-  </div>
+const loaderStyles = makeStyles(
+  (_) => createStyles(
+    {
+      root: {
+        position: 'relative',
+      },
+      bottom: {
+        color: '#dedede',
+      },
+      top: {
+        color: '#0078D4',
+        animationDuration: '1500ms',
+        position: 'absolute',
+        left: 0,
+      },
+      circle: {
+        strokeLinecap: 'round',
+      },
+    }
+  ),
+);
+
+export default function Loader(props: CircularProgressProps) {
+  const classes = loaderStyles();
+
+  return (
+    <div className={classes.root}>
+      <CircularProgress
+        variant="determinate"
+        className={classes.bottom}
+        size={30}
+        thickness={2}
+        {...props}
+        value={100}
+      />
+      <CircularProgress
+        variant="indeterminate"
+        disableShrink
+        className={classes.top}
+        classes={{
+          circle: classes.circle,
+        }}
+        size={30}
+        thickness={3}
+        {...props}
+      />
+    </div>
+  );
 }
