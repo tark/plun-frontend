dcdd0c0fefaf083bff6345d25584913be018dd06
Airon <airon@tark.pro>
2020-11-11 20:35:43 +0700

    plun_api.ts: add cool interceptor to re-login automatically

--- src/api/plun_api.ts
+++ src/api/plun_api.ts
@@ -2,127 +2,197 @@ import axios, {AxiosResponse} from 'axios';
 import moment from 'moment';
 import {UnauthorizedError} from './unauthorized_error';
 import {DATE_FORMAT} from '../config/constants';
-import {Plan, Task} from './models/models';
+import {AzureAuthResponse, Plan, Task, User} from './models/models';
+import {
+  getAccessTokenFromStorage,
+  getRefreshTokenFromStorage,
+  saveAccessTokenToStorage, saveRefreshTokenToStorage
+} from '../config/storage';
+
+export const configureApi = () => {
+  axios.defaults.headers.common.Authorization = `Bearer ${getAccessTokenFromStorage()}`;
+
+  // Add a request interceptor
+  axios.interceptors.request.use(
+    config => {
+      config.headers = {
+        Authorization: `Bearer ${getAccessTokenFromStorage()}`,
+        Accept: 'application/json',
+      }
+      return config;
+    },
+    error => {
+      // Do something with request error
+      return Promise.reject(error);
+    },
+  );
+
+  // Response interceptor for API calls
+  axios.interceptors.response.use(
+    response => response,
+    async (error) => {
+      const originalRequest = error.config;
+      // if returned error is 401 and we have refresh token
+      if (error.response.status === 401 && getRefreshTokenFromStorage()) {
+        console.log('Got 401 error and we have refresh token')
+        const azureAuthResponse = await refreshAccessToken(getRefreshTokenFromStorage());
+        const {accessToken, refreshToken} = azureAuthResponse
+        saveAccessTokenToStorage(accessToken)
+        saveRefreshTokenToStorage(refreshToken)
+        axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+        return axios(originalRequest);
+      }
+
+      console.log(`get error - ${error.response.status}`)
+      return Promise.reject(error);
+    }
+  );
+
+}
 
 const baseApiUrl = 'http://localhost:3001';
 
-export const getProfile = async (token: string) => {
-  console.log('getProfile')
-  return get('profile', {token})
+/**
+ * Ask server to auth by given code.
+ * Server auth, save access token and backend token on server
+ * side. And returns us a profile.
+ */
+export const auth = async (authCode: string): Promise<AzureAuthResponse> => {
+  console.log('api - auth')
+  return post('auth', {authCode})
 }
 
-export const getToken = async (authCode: string) => {
-  console.log('getToken')
-  return post('token', {authCode})
+export const refreshAccessToken = (refreshToken: string): Promise<AzureAuthResponse> => {
+  return post('refreshToken', {refreshToken})
 }
 
-export const authToAzureByUserId = async (userId: string) => {
-  console.log(`authToAzureByUserId - ${userId}`)
-  return post('auth', {userId})
+// --- PRIVATE ---
+
+export const getProfile = async (): Promise<User> => {
+  console.log('api - getProfile')
+  return get('profile')
 }
 
-export const getOrganizations = async (token: string) => {
-  console.log(`getOrganizations`)
-  return get('organizations', {token})
+export const getOrganizations = async () => {
+  console.log('api - getOrganizations')
+  return get('organizations')
 }
 
-export const getProjects = async (organizationName: string, token: string) => {
-  console.log(`getProjects`)
-  return get('projects', {organizationName, token})
+export const getProjects = async (organizationName: string) => {
+  console.log('api - getProjects')
+  return get('projects', {organizationName})
 }
 
 export const getSuggestions = async (
   organizationName: string,
   projectName: string,
   query: string,
-  token: string
-) : Promise<Array<Task>> => {
-  console.log(`getSuggestions - ${organizationName}, ${projectName}, ${query}`)
-  const result = await get('tasks-suggestions', {organizationName, projectName, query, token})
-  console.log(`getSuggestions - ${result}`)
+): Promise<Array<Task>> => {
+  console.log(`api - getSuggestions - ${organizationName}, ${projectName}, ${query}`)
+  const result = await get('tasks-suggestions', {
+    organizationName,
+    projectName,
+    query,
+  })
   return result
 }
 
 export const planTasks = async (tasks: Array<Task>) => {
   const result = await post('plan', {tasks})
-  console.log(`planTasks - ${result}`)
+  console.log(`api - planTasks - ${result}`)
   return result
 }
 
-export const createPlan = async (plan: Plan) => {
-  const result = await post('plan', {plan})
-  console.log(`createPlan - ${result}`)
+export const createPlan = async (
+  plan: Plan,
+  organizationName: string,
+  projectName: string,
+): Promise<Plan> => {
+  const result = await post('plan', {plan, organizationName, projectName})
+  console.log(`api - createPlan - ${result}`)
   return result
 }
 
 export const updatePlan = async (plan: Plan) => {
   const result = await patch('plan', {plan})
-  console.log(`updatePlan - ${result}`)
+  console.log(`api - updatePlan - ${result}`)
   return result
 }
 
-export const getPlanForToday = async (
-  organizationName: string,
-  projectName: string,
-  token: string
-) :Promise<Plan> => {
+export const getPlan = async (date: string, organizationName: string, projectName: string): Promise<Plan> => {
+  console.log(`getPlan - date - ${date}`)
+  const result = await get('plan', {
+    organizationName,
+    projectName,
+    date,
+  })
+  return result
+}
+
+/**
+ * @deprecated use [getPlan] instead
+ * @param organizationName
+ * @param projectName
+ */
+export const getPlanForToday = async (organizationName: string, projectName: string): Promise<Plan> => {
   const result = await get('plan', {
     organizationName,
     projectName,
     date: moment().format(DATE_FORMAT),
-    token,
   })
-  console.log(`getPlanForToday - ${result}`)
   return result
 }
 
-export const getPreviousNearestPlan = async (
-  organizationName: string,
-  projectName: string,
-  token: string
-) : Promise<Plan> => {
-  const result = await get('plan', {
+/**
+ * @deprecated use [getPlan] instead
+ * @param organizationName
+ * @param projectName
+ */
+export const getPreviousNearestPlan = async (organizationName: string, projectName: string): Promise<Plan> => {
+  const result = await get('private/plan', {
     organizationName,
     projectName,
-    token,
   })
-  console.log(`getPreviousNearestPlan - ${result}`)
   return result
 }
 
 export const deleteTask = async (task: Task) => {
-  return del('plan', {taskId: task.id})
+  return del('private/plan', {taskId: task.id})
 }
 
 export const updateTask = async (task: Task) => {
-  return patch('plan', {task})
+  return patch('api - plan', {task})
 }
 
-const post = async (endpoint: string, body: any) => {
-  console.log('post')
+// ----------
+
+const post = async (endpoint: string, body?: any) => {
+  console.log(`api - post - ${JSON.stringify(body)}`)
   const result = await axios.post(`${baseApiUrl}/${endpoint}`, body)
   return checkAndReturn(result)
 }
 
 const del = async (endpoint: string, body: any) => {
-  console.log('del')
+  console.log('api - del')
   const result = await axios.delete(`${baseApiUrl}/${endpoint}`, {data: body})
   return checkAndReturn(result)
 }
 
-const patch = async (endpoint:string, body: any) => {
-  console.log('patch')
+const patch = async (endpoint: string, body: any) => {
+  console.log('api - patch')
   const result = await axios.patch(`${baseApiUrl}/${endpoint}`, body)
   return checkAndReturn(result)
 }
 
-const get = async (endpoint: string, params: any): Promise<any> => {
-  console.log('get')
+const get = async (endpoint: string, params?: any): Promise<any> => {
+  console.log(`api - get - ${baseApiUrl}/${endpoint}, ${JSON.stringify(params)}`)
   const result: AxiosResponse = await axios.get(`${baseApiUrl}/${endpoint}`, {params})
   return checkAndReturn(result)
+
 }
 
 const checkAndReturn = (result: AxiosResponse): any => {
+  console.log('checkAndReturn')
   switch (result.status) {
     case 401:
       throw new UnauthorizedError()
@@ -131,3 +201,4 @@ const checkAndReturn = (result: AxiosResponse): any => {
       return result.data;
   }
 }
+
