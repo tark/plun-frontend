dc34f40868e5c411ef8d79986ad272163ee73c19
Airon <airon@tark.pro>
2020-11-11 20:39:15 +0700

    TaskItem.tsx: code refactor

--- src/views/landing/task_item/TaskItem.tsx
+++ src/views/landing/task_item/TaskItem.tsx
@@ -1,47 +1,32 @@
 import React, {useState} from 'react'
 import {IconButton} from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import './task_item.css';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 import classnames from 'classnames';
-import {Moment} from 'moment';
-import {iconByTaskState} from '../../../util/task_util';
+import moment from 'moment';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {
+  faHammer,
+  faCheckSquare,
+  faTimes,
+  faTrash,
+  faCircle
+} from '@fortawesome/free-solid-svg-icons'
+import {IconDefinition} from '@fortawesome/fontawesome-common-types';
 import {Task, TaskState} from '../../../api/models/models';
+import {DATE_FORMAT, TaskStatus} from '../../../config/constants';
 
 const TITLE_MAX_LENGTH = 42
-
-const options = [
-  {
-    state: 'created',
-    label: '🔘 Created'
-  },
-  {
-    state: 'progress',
-    label: '🚧 Progress'
-  },
-  {
-    state: 'done',
-    label: '✅ Finished'
-  },
-  {
-    state: 'failed',
-    label: '❌ Not started'
-  },
-  {
-    state: 'cancelled',
-    label: '🗑️ Cancelled'
-  },
-]
 
 interface TaskItemProps {
   task: Task;
   state: TaskState;
-  date: Moment;
-  isLocal: boolean;
-  showDeleteButton: boolean;
-  onDeletePressed: Function;
-  onStateChanged: Function;
+  date: string;
+  taskInNextPlan: boolean;
+  onDeletePressed: (task: Task) => void;
+  onCopyToNexPlanPressed: (task: Task) => void;
+  onStateChanged: (task: Task, state: TaskState) => void;
   stateChanging: boolean;
   showState: boolean;
 }
@@ -50,27 +35,16 @@ export default function TaskItem(props: TaskItemProps) {
 
   const {
     task,
-    showDeleteButton,
+    date,
     showState,
     onDeletePressed,
-    onStateChanged,
-    isLocal,
-    date,
     state,
-    stateChanging,
+    taskInNextPlan,
+    onCopyToNexPlanPressed,
   } = props
 
-  const {name} = task
-  console.log(`TaskItem - ${JSON.stringify(task)}`)
-  const [menuAnchor, setMenuAnchor] = useState<HTMLDivElement | null>(null);
-
-  /*const stateView = () => {
-    return <div
-      className='status-icon'
-      onClick={e => setMenuAnchor(e.currentTarget)}>
-      {iconByTaskState(state.find(s => s.date === date.format(DATE_FORMAT))?.state)}
-    </div>
-  }*/
+  const {name, azureUrl} = task
+  const [showActions, setShowActions] = useState(false)
 
   const titleClipped = () => {
 
@@ -84,59 +58,80 @@ export default function TaskItem(props: TaskItemProps) {
     return `${name.substr(0, TITLE_MAX_LENGTH)}...`
   }
 
-  const handleMenuClose = () => {
-    setMenuAnchor(null);
-  };
+  const canMoveTaskToNextPlan = (): boolean => {
+    return !taskInNextPlan && (state === TaskStatus.created || state === TaskStatus.progress) && date !== moment().format(DATE_FORMAT)
+  }
 
-  const handleMenuItemClick = (event: any, option: any) => {
-    onStateChanged(option.state);
-    setMenuAnchor(null);
-  };
+  const iconByTaskState = (taskState: TaskState): IconDefinition => {
+    console.log(`iconByTaskState - ${taskState}`)
+    switch (taskState) {
+      case 'created':
+      default:
+        return faCircle
+      case 'done':
+        return faCheckSquare
+      case 'progress':
+        return faHammer
+      case 'failed':
+        return faTimes
+      case 'cancelled':
+        return faTrash
+    }
+  }
 
-  /*const taskState = () : TaskState => {
-    console.log(`taskState - ${JSON.stringify(state)}`)
-    console.log(`taskState - ${date.format(DATE_FORMAT)}`)
-    return state.find(s => s.date === date.format(DATE_FORMAT))?.state ?? 'created';
-  }*/
+  const actionStateButton = (taskState: TaskState) => {
+    return <div className='action-icon'>
+      <FontAwesomeIcon icon={iconByTaskState(taskState)}/>
+    </div>
+  }
 
-  return <div className='d-flex flex-row task'>
+  return <div
+    className='d-flex flex-row task'
+    onMouseEnter={() => setShowActions(true)}
+    onMouseLeave={() => setShowActions(false)}>
 
-    {showState && <div
-      className='status-icon'
-      onClick={e => setMenuAnchor(e.currentTarget)}>
-      {iconByTaskState(state)}
-    </div>}
+    <div className='status-icon-container'>
+
+      {state === TaskStatus.created && <div className='status-icon icon-bullet'>
+        <FontAwesomeIcon icon={iconByTaskState(state)}/>
+      </div>}
+
+      {showState && state !== TaskStatus.created && <div
+        className='status-icon'>
+        <FontAwesomeIcon icon={iconByTaskState(state)}/>
+      </div>}
+    </div>
 
     <div
       className={classnames(
         'task-title flex-grow-1',
-        {'task-title-local': isLocal}
+        {'task-title-local': !!azureUrl}
       )}>
-      {titleClipped()}
+      {azureUrl && <a href={azureUrl}>{titleClipped()}</a>}
+      {!azureUrl && titleClipped()}
     </div>
 
-    {showDeleteButton && !isLocal && <div>
+
+    {showActions && date === moment().format(DATE_FORMAT) && <div>
       <IconButton size='small' onClick={() => onDeletePressed(task)}>
-        <DeleteIcon fontSize='small' className='status-icon' style={{color: '#bbb'}}/>
+        <DeleteIcon fontSize='small' className='action-icon'/>
       </IconButton>
     </div>}
 
-    <Menu
-      id="lock-menu"
-      anchorEl={menuAnchor}
-      keepMounted
-      open={Boolean(menuAnchor)}
-      onClose={handleMenuClose}>
-      {options.map((option, index) => (
-        <MenuItem
-          key={option.label}
-          disabled={state === option.state}
-          selected={state === option.state}
-          onClick={(event) => handleMenuItemClick(event, option)}>
-          {option.label}
-        </MenuItem>
-      ))}
-    </Menu>
+    {showActions && canMoveTaskToNextPlan() && <div>
+      <IconButton size='small' onClick={() => onCopyToNexPlanPressed(task)}>
+        <ArrowDownwardIcon fontSize='small' className='action-icon'/>
+      </IconButton>
+    </div>}
+
+    {showActions && actionStateButton('failed')}
+
+    {showActions && actionStateButton('progress')}
+
+    {showActions && actionStateButton('cancelled')}
+
+    {showActions && actionStateButton('done')}
+
   </div>
 
 }
