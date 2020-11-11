64a39689141969e06d2ee8924a5893e5fb5b8d6e
Airon <airon@tark.pro>
2020-11-11 20:32:21 +0700

    ProjectsList.tsx: implement new Redux logic

--- src/views/main/ProjectsList.tsx
+++ src/views/main/ProjectsList.tsx
@@ -1,53 +1,45 @@
-import {object, string, func} from 'prop-types'
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
 import './main.css';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import {getProjects} from '../../api/plun_api';
+import {useDispatch, useSelector} from 'react-redux';
 import Loader from '../components/loader';
-import {Organization, Project} from '../../api/models/models';
-
+import {Organization} from '../../api/models/models';
+import {profileSelectors, selectProject} from '../../store/slices/profile_slice';
+import {fetchProjects} from '../../services/profile_service';
 
 interface ProjectsListProps {
-  token: string;
   organization: Organization;
-  onChange: Function;
 }
 
 export default function ProjectsList(props: ProjectsListProps) {
-
-  const [projects, setProjects] = useState<Array<Project>>()
-  const [fetchingProjects, setFetchingProjects] = useState(false)
-  const [selectedProject, setSelectedProject] = useState<Project>()
-
-  const {token, organization, onChange} = props
+  const dispatch = useDispatch()
+  const projects = useSelector(profileSelectors.projects)
+  const projectsLoading = useSelector(profileSelectors.projectsLoading)
+  const selectedOrganization = useSelector(profileSelectors.selectedOrganization)
+  const selectedProject = useSelector(profileSelectors.selectedProject)
+  const {organization} = props
 
   useEffect(() => {
-    fetchProjects().then()
-  }, [token, organization])
+    dispatch(fetchProjects(organization.name))
+  }, [])
 
-  const fetchProjects = async () => {
-    console.log(`fetchProjects - ${organization.name}`)
-    setFetchingProjects(true)
-    const myProjects = await getProjects(organization.name, token);
-    setProjects(myProjects);
-    selectProject(myProjects[0]);
-    setFetchingProjects(false)
-  }
 
-  const selectProject = (p: Project) => {
-    setSelectedProject(p)
-    if (onChange) {
-      onChange(p)
+  useEffect(() => {
+    // once projects is loaded and our organization is the selected - we will select
+    // first projects from the given
+    if (projects && organization.id === selectedOrganization?.id) {
+      dispatch(selectProject(projects[0]))
     }
-  }
+  }, [projects])
+
 
   return (
     <div>
 
-      {fetchingProjects && <Loader/>}
+      {projectsLoading && <Loader/>}
 
       {projects && !projects.length && (
         <div>No projects for this organization</div>
@@ -58,7 +50,7 @@ export default function ProjectsList(props: ProjectsListProps) {
           <ListItem
             button
             key={`${p.id}-item`}
-            onClick={(e) => selectProject(p)}
+            onClick={(e) => dispatch(selectProject(p))}
             selected={selectedProject?.azureId === p.azureId}
           >
             <ListItemIcon/>
