a387cca652edb4da5bdea45fc573056e5fab6c3c
Airon <airon@tark.pro>
2020-11-21 12:18:51 +0700

    store/index: add slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,16 @@ import {configureStore} from '@reduxjs/toolkit'
 import profileReducer from './slices/profile_slice'
 import planReducer from './slices/plan_slice'
 import tasksReducer from './slices/tasks_slice'
+import usersReducer from './slices/users_slice'
+import errorReducer from './slices/error_slice'
 
 export const store = configureStore({
   reducer: {
     profile: profileReducer,
     plan: planReducer,
     tasks: tasksReducer,
+    users: usersReducer,
+    error: errorReducer,
   },
 })
 
